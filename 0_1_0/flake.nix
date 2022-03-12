197acaef59b759afef98b5b05eaa5c6dc2401e06
hugosenari <hugosenari@gmail.com>
2022-03-12 01:36:38 -0300

    chore: re index vmprotect

---
+++ 0_1_0/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Wrapper for VMProtect SDK'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-vmprotect-0_1_0.flake = false;
+  inputs.src-vmprotect-0_1_0.owner = "ba0f3";
+  inputs.src-vmprotect-0_1_0.ref   = "refs/tags/0.1.0";
+  inputs.src-vmprotect-0_1_0.repo  = "vmprotect.nim";
+  inputs.src-vmprotect-0_1_0.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-vmprotect-0_1_0"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-vmprotect-0_1_0";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
